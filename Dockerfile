cfe3e0f2a8f170429af6abffead82e54ad60b653
Mohd Azam Ansari <azamansari20may2004@gmail.com>
2025-05-30 22:46:42 +0530

    increase timeout

--- Dockerfile
+++ Dockerfile
@@ -18,4 +18,5 @@ RUN python manage.py migrate
 EXPOSE 8000
 
 # Start the app
-CMD ["gunicorn", "product_scraper.wsgi:application", "--bind", "0.0.0.0:8000"]
+CMD ["gunicorn", "product_scraper.wsgi:application", "--bind", "0.0.0.0:8000", "--timeout", "250"]
+
--- product_scraper/settings.py
+++ product_scraper/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0+-cjcrr95!-y$)k)$l042780+rujhl4c@%-uxf$0$d6=n+@1w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["product-scraper-api-3okm.onrender.com", "localhost" ]
+ALLOWED_HOSTS = ["product-scraper-api-3okm.onrender.com", "localhost" ,"127.0.0.1"]
 
 
 # Application definition
